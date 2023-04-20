848dba9f4ee672f4e5b4b9d20e49901d3696588e
pyush <psinha5@horizon.csueastbay.edu>
2023-04-20 10:28:03 -0700

    4-20-2023 | frontend

--- code/app.py
+++ code/app.py
@@ -4,17 +4,18 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 text = ""
-comments, urls = API_test.recentComments(5, API_test.reddit, API_test.target_user)
 @app.route('/') #This is the function that is run when the home page is visited 
 def home():
-    their_comments = comments
-    return render_template('index.html', posts=their_comments)
+    return render_template('index.html')
 
 @app.route('/submit', methods=['POST']) #This is the function that is run when the /submit page is visited
 def submit():
     global text
     text = request.form['text']
-    return 'Text received!' + text
+    comments, urls = API_test.recentComments(5, API_test.reddit, text)
+    their_comments = comments
+    return render_template('index.html', posts=their_comments)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
