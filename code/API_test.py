2d9b0be48d11070e35a2bfd3f3b9559af3eacaa2
pyush <psinha5@horizon.csueastbay.edu>
2023-04-19 21:30:32 -0700

    4/19/2023 9:30pm

--- code/API_test.py
+++ code/API_test.py
@@ -43,17 +43,17 @@ def printIndices(indices_list, original_list):
 
 
 
-topSub(10, reddit, subreddit) # Test 1
-comments, urls = recentComments(1500, reddit, target_user)
-my_indices = []
-isWord("gane", comments, my_indices)
-isWord("Gane", comments, my_indices)
-print(my_indices)
+# topSub(10, reddit, subreddit) # Test 1
+# comments, urls = recentComments(1500, reddit, target_user)
+# my_indices = []
+# isWord("gane", comments, my_indices)
+# isWord("Gane", comments, my_indices)
+# print(my_indices)
 
 # for i, _ in enumerate(my_indices):
 #     print( "#", i+1, "overall comment #", my_indices[i], "START",  comments[my_indices[i]], "END")
 
-printIndices(my_indices, comments)
+# printIndices(my_indices, comments)
 # print("**Test 3: SEARCHING FOR A PHRASE IN ONE COMMENT**")
 # sentence = "Is punch in this sentence?"
 # word = "punch"
@@ -70,19 +70,19 @@ printIndices(my_indices, comments)
 #         print(word, "was found in comment #", comments.index(comment)+1)
 # print("**End of Test 4**")
 
-print("**Test 5: RUNNING SENTIMENT ANALYSIS ON A COMMENT**")
-text = comments[468]
-text = SA_test.preprocess(text)
-encoded_input = SA_test.tokenizer(text, return_tensors='pt')
-output = SA_test.model(**encoded_input)
-scores = output[0][0].detach().numpy()
-scores = SA_test.softmax(scores)
-ranking = SA_test.np.argsort(scores)
-ranking = ranking[::-1]
-for i in range(scores.shape[0]):
-    l = SA_test.labels[ranking[i]]
-    s = scores[ranking[i]]
-    print(f"{i+1}) {l} {SA_test.np.round(float(s), 4)}")
-print("**End of Test 5 **")
+# print("**Test 5: RUNNING SENTIMENT ANALYSIS ON A COMMENT**")
+# text = comments[468]
+# text = SA_test.preprocess(text)
+# encoded_input = SA_test.tokenizer(text, return_tensors='pt')
+# output = SA_test.model(**encoded_input)
+# scores = output[0][0].detach().numpy()
+# scores = SA_test.softmax(scores)
+# ranking = SA_test.np.argsort(scores)
+# ranking = ranking[::-1]
+# for i in range(scores.shape[0]):
+#     l = SA_test.labels[ranking[i]]
+#     s = scores[ranking[i]]
+#     print(f"{i+1}) {l} {SA_test.np.round(float(s), 4)}")
+# print("**End of Test 5 **")
 
 
--- code/app.py
+++ code/app.py
@@ -1,12 +1,14 @@
+import API_test
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 text = ""
-
+comments, urls = API_test.recentComments(5, API_test.reddit, API_test.target_user)
 @app.route('/') #This is the function that is run when the home page is visited 
 def home():
-    return render_template('index.html')
+    their_comments = comments
+    return render_template('index.html', posts=their_comments)
 
 @app.route('/submit', methods=['POST']) #This is the function that is run when the /submit page is visited
 def submit():
