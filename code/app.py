d94e56f99d6110de3bf60942e776ea3daba5b559
pyush sinha <pyushsinha20@gmail.com>
2023-07-27 19:56:29 +0530

    URLS added to comments 7/27/2023

--- code/app.py
+++ code/app.py
@@ -16,20 +16,28 @@ def submit():
     indices = []
     matches = []
     urls = []
+    matchURLs = []
     toplabels = []
     # SAword = str()
     SAlistoflists = []
     text = request.form['text']
     text2 = request.form['text2']
     found = True
+    #Searches for phrase in comment history of a username
     their_comments, urls, indices = API_test.search(API_test.reddit, text, text2)
     for i in range(len(indices)):
         matches.append(their_comments[indices[i]])
-    
+        matchURLs.append("https://reddit.com" + urls[indices[i]])
+    #If invalid username or no matching comments
     if not matches:
         found = False
-        matches.append(their_comments[0])
-    
+        if their_comments[0] == "No comments for invalid username":
+            matches.append(their_comments[0])
+            matchURLs.append(urls[0])
+        else:
+            matches.append("No matching comments found")
+            matchURLs.append("#!")
+    #Run SA on all matching comments
     if found:
         print("**Test 5: RUNNING SENTIMENT ANALYSIS ON A COMMENT**")
         for i, _ in enumerate(matches):
@@ -41,7 +49,7 @@ def submit():
             # SAlistoflists.append(SAword)
 
     print(SAlistoflists)
-    return render_template('demo2.html', posts=matches, SAlist = SAlistoflists, labels = toplabels) + text2
+    return render_template('demo2.html', posts=matches, SAlist = SAlistoflists, labels = toplabels, hyperlinks = matchURLs) + text2
 
 
 if __name__ == '__main__':
